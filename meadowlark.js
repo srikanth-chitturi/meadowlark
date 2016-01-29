b94b424934e69f9325acc8fc4616a1ada4ade6f7
srikanth-chitturi <ysrikanth2011@gmail.com>
2016-01-29 16:32:44 +0530

    Added views and view engine

--- meadowlark.js
+++ meadowlark.js
@@ -2,13 +2,28 @@ var express = require('express');
 
 var app = express();
 
-app.use('port',process.env.PORT || 3000);
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars',handlebars.engine);
+
+app.set('view engine','handlebars');
+
+app.set('port',process.env.PORT || 3000);
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',function(req,res){
+	res.render('home');
+});
+
+app.get('/about',function(req,res){
+	res.render('about');
+});
 
 //custom 404 page
 app.use(function  (req,res) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not found');
+	res.render('404');
 });
 
 //custom 500 page
@@ -16,10 +31,11 @@ app.use(function(err,req,res,next){
 	console.log(err.stack);
 	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
-app.listen(app.get('port'),function(){
-	console.log('Express Server running on http://localhost://'+app.get('port')+'; press 
-		ctrl+c to terminate');
+app.listen(3000,function(){
+	console.log('Express Server running on http://localhost://' +app.get('port')+
+		'; press Ctrl+C to terminate');
 });
+
